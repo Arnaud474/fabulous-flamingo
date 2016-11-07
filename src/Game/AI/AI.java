d4176c921523e62853f1c93b8577004f1eb793b3
Arnaud Girardin <arnaud.girardin444@gmail.com>
2016-11-07 13:18:03 -0500

    Moves possibles

--- src/Game/AI/AI.java
+++ src/Game/AI/AI.java
@@ -3,6 +3,7 @@ package Game.AI;
 import Game.Board;
 import Game.Piece;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class AI {
@@ -28,7 +29,7 @@ public class AI {
      * @param board
      * @return
      */
-    private static ArrayList<Move> findAllPossibleMoves(int color, Board board){
+    private ArrayList<Move> findAllPossibleMoves(int color, Board board){
 
         System.out.println("All Moves");
 
@@ -39,10 +40,41 @@ public class AI {
         for(int i = 0; i < b.length; i++){
 
             for(int j = 0; j < board.getBoard()[0].length; j++){
+
                 //Check if spot contains Piece of the good color
                 if(b[i][j] != null && b[i][j].getColor() == color)
                 {
+                    //To contain Piece of another color in the same direction
+                    Point obstacle = null;
+                    int count = 0;
+
                     //Check number of pieces in the same ROW/COLUMN (Check if blocked by another color)
+
+                    //Horizontal check on all spaces
+                    for(int x = 0; x < b[i].length; x++){
+
+                        //If spot inside the i ROW contains a Piece
+                        if(b[i][x] != null) {
+                            count++;
+
+                            //If the Piece is of not our color, it is an obstacle
+                            if(b[i][x].getColor() != color && obstacle == null){
+                                obstacle = new Point(x, i);
+                            }
+                        }
+                    }
+
+                    //Movement validation
+
+                    //FORWARD MOVE (RIGHT)
+                    if((j+count) < b[i].length){
+                        //Check for obstacles
+                        if(obstacle != null && (j+1) <= obstacle.getX()){
+                            moves.add(new Move(b[i][j], new Point(j, i), new Point(j+count, i)));
+                        }
+                    }
+
+
 
                     //Check diagonals (Check if blocked by another color)
 
@@ -68,7 +100,7 @@ public class AI {
     public static String findBestMove(int color, Board board){
 
         //Find all moves
-        ArrayList<Move> moves = findAllPossibleMoves(color, board);
+        ArrayList<Move> moves = INSTANCE.findAllPossibleMoves(color, board);
 
         return "";
     }
