bb0174285ab44f28ec9352f25c4165e826d75ae6
Arnaud Girardin <arnaud.girardin444@gmail.com>
2016-11-16 16:12:08 -0500

    Fix signature de methode

--- src/Game/AI/Heuristic/Strategies/AggressiveStrategy.java
+++ src/Game/AI/Heuristic/Strategies/AggressiveStrategy.java
@@ -1,6 +1,9 @@
 package Game.AI.Heuristic.Strategies;
 
+import Game.AI.Move;
 import Game.Board;
+
+import java.util.ArrayList;
 
 /**
  * Created by Arnaud on 11/16/2016.
@@ -8,7 +11,7 @@ import Game.Board;
 public class AggressiveStrategy extends Strategy {
 
     @Override
-    public void calculateValues(Board board) {
+    public void calculateValues(Board board, ArrayList<Move> moves) {
 
     }
 }
